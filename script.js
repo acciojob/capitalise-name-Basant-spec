b7bf59e39e476c225f6d9ba69fe48184d6ee8089
Basant-spec <basantdhiman9@gmail.com>
2024-11-16 12:13:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-//your JS code here. If required.
- function capitalizeName() {
-            const inputField = document.getElementById("fname");
-            inputField.value = inputField.value.toUpperCase();
-        }
+// Function to capitalize the input value when the input field loses focus
+function capitalizeName() {
+  const inputField = document.getElementById('fname');
+  inputField.value = inputField.value.toUpperCase();
+}
+
+// Add event listener for the 'onblur' event
+document.getElementById('fname').addEventListener('blur', capitalizeName);
